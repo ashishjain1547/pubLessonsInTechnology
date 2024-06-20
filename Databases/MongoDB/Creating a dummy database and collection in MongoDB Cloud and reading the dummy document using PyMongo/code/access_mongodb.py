e59d34a0239c9728ee083243e9b8e23d682b4c70
ashish <ashishjain1547@gmail.com>
2024-06-20 15:42:24 +0530

    #

---
+++ Databases/MongoDB/Creating a dummy database and collection in MongoDB Cloud and reading the dummy document using PyMongo/code/access_mongodb.py
@@ -0,0 +1,8 @@
+
+# Dated: 2022-Sep-5
+# For Python 3.6 or later (< 3.11)
+
+import pymongo 
+client = pymongo.MongoClient("mongodb+srv://ashishjain:password@123@cluster0.btzugx2.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
